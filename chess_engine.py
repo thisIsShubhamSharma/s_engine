41f8413c6929a426fbc7134ef453052116c7eadb
Shubham Sharma <klusbn@gmail.com>
2021-03-05 17:21:19 +0530

    added functionality of moving pieces.

--- chess_engine.py
+++ chess_engine.py
@@ -29,5 +29,35 @@ class GameState():
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
         ]
 
-        self.whiteToMove = True
-        self.MoveLog = []
+        self.white_to_move = True
+        self.move_log = []
+
+    def make_move(self, move):
+        self.board[move.start_row][move.start_col] = "--"
+        self.board[move.end_row][move.end_col] = move.piece_moved
+        self.move_log.append(move)  # log moves
+        self.white_to_move = not self.white_to_move  # change turn
+
+
+class Move():
+    ranks_to_rows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                     "5": 3, "6": 2, "7": 1, "8": 0}
+    rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
+    files_to_cols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                     "e": 4, "f": 5, "g": 6, "h": 7}
+    cols_to_files = {v: k for k, v in files_to_cols.items()}
+
+    def __init__(self, start_sq, end_sq, board):
+        self.start_row = start_sq[0]
+        self.start_col = start_sq[1]
+        self.end_row = end_sq[0]
+        self.end_col = end_sq[1]
+
+        self.piece_moved = board[self.start_row][self.start_col]
+        self.piece_captured = board[self.end_row][self.end_col]
+
+    def get_chess_notation(self):
+        return self.get_rank_file(self.start_row, self.start_col) + self.get_rank_file(self.end_row, self.end_col)
+
+    def get_rank_file(self, r, c):
+        return self.cols_to_files[c] + self.rows_to_ranks[r]
--- chess_main.py
+++ chess_main.py
@@ -43,11 +43,32 @@ def main():
     gs = chess_engine.GameState()
     load_images()
     running = True
+    sq_selected = ()  # keeps track of last user click. tuple(row,col)
+    # keeps track of user clicks. list[tuple(row,col), tuple(row,col)]
+    player_clicks = []
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
-
+            if e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()  # (x,y) location of mouse
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sq_selected == (row, col):  # user clicked the same square
+                    sq_selected = ()  # clear selection
+                    player_clicks = []  # clear the list
+                else:
+                    sq_selected = (row, col)
+                    # append the clicks to the list
+                    player_clicks.append(sq_selected)
+                if len(player_clicks) == 2:  # 1st and 2nd click
+                    move = chess_engine.Move(
+                        player_clicks[0], player_clicks[1], gs.board)
+                    # prints the move in chess notation
+                    print(move.get_chess_notation())
+                    gs.make_move(move)
+                    sq_selected = ()
+                    player_clicks = []
         draw_game_state(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
@@ -59,13 +80,24 @@ def draw_game_state(screen, gs):
     draw_pieces(screen, gs.board)
 
 
+'''
+Draws the board.
+'''
+
+
 def draw_board(screen):
     colors = [p.Color("white"), p.Color("gray")]
     for row in range(DIMENSION):
         for column in range(DIMENSION):
+            # adds rows and columns gets reminder if reminder is 1 draws a dark block else if reminder is 0 draws white.
             color = colors[((row+column) % 2)]
             p.draw.rect(screen, color, p.Rect(
                 column*SQ_SIZE, row*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+'''
+Draws the pieces on board.
+'''
 
 
 def draw_pieces(screen, board):
